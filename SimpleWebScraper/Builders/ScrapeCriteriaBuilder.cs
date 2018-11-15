d301d05aec7b0c67ab10d11cc254eea61a0e6ed1
Prateek Chaplot <prateekchaplot96@gmail.com>
2018-11-15 22:10:20 +0530

    Builder files added

---
+++ SimpleWebScraper/Builders/ScrapeCriteriaBuilder.cs
@@ -0,0 +1,61 @@
+﻿using SimpleWebScraper.Data;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper.Builders
+{
+    class ScrapeCriteriaBuilder
+    {
+        public string _data;
+        public string _regex;
+        public RegexOptions _regexOption;
+        public List<ScrapeCriteriaPart> _parts;
+
+        public ScrapeCriteriaBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _data = string.Empty;
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+            _parts = new List<ScrapeCriteriaPart>();
+        }
+
+        public ScrapeCriteriaBuilder WithData(string data)
+        {
+            _data = data;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithParts(ScrapeCriteriaPart parts)
+        {
+            _parts.Add(parts);
+            return this;
+        }
+
+        public ScrapeCriteria Build()
+        {
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteria();
+            scrapeCriteria.Data = _data;
+            scrapeCriteria.Regex = _regex;
+            scrapeCriteria.RegexOption = _regexOption;
+            scrapeCriteria.Parts = _parts;
+            return scrapeCriteria;
+        }
+    }
+}
---
+++ SimpleWebScraper/Builders/ScrapeCriteriaPartBuilder.cs
@@ -0,0 +1,42 @@
+﻿using SimpleWebScraper.Data;
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper.Builders
+{
+    class ScrapeCriteriaPartBuilder
+    {
+        public string _regex;
+        public RegexOptions _regexOption;
+
+        public ScrapeCriteriaPartBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaPart Build()
+        {
+            ScrapeCriteriaPart scrapeCriteriaPart = new ScrapeCriteriaPart();
+            scrapeCriteriaPart.Regex = _regex;
+            scrapeCriteriaPart.RegexOption = _regexOption;
+            return scrapeCriteriaPart;
+        }
+    }
+}
