9fe6e22cb789802d9ab0da25e19a76367157d4d7
Prateek Chaplot <prateekchaplot96@gmail.com>
2018-11-15 21:30:02 +0530

    Data files added

---
+++ SimpleWebScraper/Data/ScrapeCriteria.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper.Data
+{
+    class ScrapeCriteria
+    {
+        public string Data { get; set; }
+        public string Regex { get; set; }
+        public RegexOptions RegexOption { get; set; }
+        public List<ScrapeCriteriaPart> Parts { get; set; }
+
+        public ScrapeCriteria()
+        {
+            Parts = new List<ScrapeCriteriaPart>();
+        }
+    }
+}
